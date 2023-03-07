9894a57e0c6bdf3263938ef30f34fb2794879fbb
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2023-03-07 20:02:11 +0100

    Add testdata

---
+++ testdata/generateTestdata.php
@@ -0,0 +1,67 @@
+<?php
+
+$numbers = [
+    [
+        'number' => '5560160680',
+        'type'   => 'Aktiebolag',
+        'valid'  => true,
+    ],
+    [
+        'number' => '5561034249',
+        'type'   => 'Aktiebolag',
+        'valid'  => true,
+    ],
+    [
+        'number' => '5561034249',
+        'type'   => 'Aktiebolag',
+        'valid'  => true,
+    ],
+    [
+        'number' => '5592440001',
+        'type'   => 'Aktiebolag',
+        'valid'  => true,
+    ],
+    [
+        'number' => '5560160681',
+        'type'   => 'Aktiebolag',
+        'valid'  => false,
+    ],
+    [
+        'number' => '5561034250',
+        'type'   => 'Aktiebolag',
+        'valid'  => false
+    ],
+    [
+        'number' => '121212121212',
+        'type'   => 'Enskild firma',
+        'valid'  => true,
+    ],
+];
+
+$list = [];
+
+function createListObjectItem(array $number)
+{
+    $shortFormat = preg_match('/^16/', $number['number']) ? substr($number['number'], 2) : $number['number'];
+
+    return [
+        'input'            => $number['number'],
+        'long_format'      => preg_replace('/(\d{4})$/', '-$1', $shortFormat, 1),
+        'short_format'     => $shortFormat,
+        'valid'            => $number['valid'],
+        'type'             => $number['type'],
+        'vat_number'       => sprintf('SE%s01', $shortFormat),
+    ];
+}
+
+foreach ($numbers as $number) {
+    $list[] = createListObjectItem($number);
+
+    if ($number['type'] !== 'Enskild firma') {
+        $list[] = createListObjectItem(array_merge($number, [
+            'number' => sprintf('16%s',$number['number']),
+        ]));
+    }
+}
+
+file_put_contents(__DIR__ . "/list.json", json_encode($list, JSON_PRETTY_PRINT));
